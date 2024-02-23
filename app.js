4ed2578712d1ec00b51e5e8580e1941c66a79afb
Shahab Uddin Ghori <shahabnisar918@gmail.com>
2024-02-23 15:53:29 +0500

    basic Am operation and linear equ

---
+++ app.js
@@ -0,0 +1,40 @@
+// Taking input from user as name via prompt and giving output via console/Alert
+let userName = prompt("What is Your Name");
+alert("Welcome Mr. " + userName);
+console.log("Welcome Mr. " + userName);
+
+// Taking Input From User using declaring a varing by prompt
+let a = parseFloat(prompt("Enter the Value of a"));
+let b = parseFloat(prompt("Enter the value of b"));
+let c = parseFloat(prompt("Enter the value of c"));
+
+let Sum = a + b + c;
+let Diff = a - b - c;
+let Mul = a * b * c;
+let Div = a / b / c;
+
+// Giving OutPut on Alert
+alert("Sum of a, b, and c is " + Sum);
+alert("Diff of a, b, and c is " + Diff);
+alert("Mul of a, b, and c is " + Mul);
+alert("Div of a, b, and c is " + Div);
+
+// giving Output on Consloe
+console.log("Sum of a, b, and c is " + Sum);
+console.log("Diff of a, b, and c is " + Diff);
+console.log("Mul of a, b, and c is " + Mul);
+console.log("Div of a, b, and c is " + Div);
+
+// giving input for Linear Equation
+alert("Find the root of Linear Equationn\n xy + z");
+console.log("Find the root of Linear Equation  xy+ z");
+let x = parseFloat(prompt("Enter the value of x"));
+let y = parseFloat(prompt("Enter the value of y"));
+let z = parseFloat(prompt("Enter the value of z"));
+
+// taking output for linear Equation
+let Linear = x*y + z;
+alert("root of xy + z is " + Linear);
+console.log("root of xy + z is   " + Linear);
+
+
